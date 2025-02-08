21da1c6a398e61ccc5344ae6976e29cc084641ea
AidrinMorris <aidrinmorris12@gmail.com>
2025-02-08 12:42:12 -0800

    Update Mod5.py

--- Mod5.py
+++ Mod5.py
@@ -1,13 +1,13 @@
-#220
+#1
 for number in range(2, 21, 2):
     print(number)
 
-#Define
+#2
 my_string = "Olympic College"
 for char in my_string:
     print(char.upper())
 
-#Loopy
+#3
 n = int(input("Enter a positive integer: "))
 while n <= 0:
     n = int(input("Please enter a positive integer: "))
@@ -18,13 +18,13 @@ for i in range(1, n + 1):
 
 print("The sum of integers from 1 to", n, "is:", total_sum)
 
-#Multiplication-Table
+#4
 for i in range(1, 6):
     for j in range(1, 6):
         print(f"{i * j:2}", end="   ")
     print()
 
-#Zero!
+#5
 while True:
     number = int(input("Enter a number (0 to stop): "))
     if number == 0:
